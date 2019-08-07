256da66b44f97f5ff167111264615296127cfc0f
Linhurdos <teremdev@gmail.com>
2019-08-08 00:09:23 +0300

    Add license

--- build.js
+++ build.js
@@ -1,3 +1,24 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+
+ * Copyright (C) 2019 Roman Erdyakov (Linhurdos) <teremdev@gmail.com>
+
+ * This file is part of Memedar (flashcard system)
+ * Memedar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 function copy_dependencies(dst_dir, dependencies)
 {
 	let ncp = require("ncp").ncp;
