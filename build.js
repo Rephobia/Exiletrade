ffec0c934a4d9cb7df4b8019bac4f067a28e72f2
Linhurdos <teremdev@gmail.com>
2019-08-08 00:13:22 +0300

    Add license file

--- build.js
+++ build.js
@@ -3,8 +3,8 @@
 
  * Copyright (C) 2019 Roman Erdyakov (Linhurdos) <teremdev@gmail.com>
 
- * This file is part of Memedar (flashcard system)
- * Memedar is free software: you can redistribute it and/or modify
+ * This file is part of Exiletrade.
+ * Exiletrade is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
